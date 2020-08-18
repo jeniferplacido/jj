b7749b427ac6a04bc6ca4996d9d088cf5babc701
Jenifer Placido <jenifer.sdti@gmail.com>
2020-08-17 21:32:19 -0300

    MercadoJJ

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 function login(form){
 form.nome.value= form.nome.value.toLowerCase()
 form.senha.value= form.senha.value.toLowerCase()
-
+alert("Cliquei")
 if(form.nome.value=="Jenifer" && form.senha.value=="1234" ||form.nome.value=="Jonathan" && form.senha.value=="12345"){
     location="login.html"
 }else{
